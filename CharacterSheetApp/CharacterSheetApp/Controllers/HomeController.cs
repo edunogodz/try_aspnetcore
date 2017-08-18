dd4ca5dcd539a5ca97d76147c2c44b79362098a8
edunogodz <edunogodz@gmail.com>
2017-08-18 20:43:45 -0300

    First Commit

---
+++ CharacterSheetApp/CharacterSheetApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CharacterSheetApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CharacterSheetApp.Models.Character();
+            model.Name = "Hux";
+            return View(model);
+        }
+
+    }
+}
