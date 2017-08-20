98cd2c0ef10b2ab4d2f0a76e95d1c34fca6cfe42
edunogodz <edunogodz@gmail.com>
2017-08-19 21:23:23 -0300

    Lesson 1

---
+++ CharacterSheetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CharacterSheetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
